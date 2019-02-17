97a957dbe458be980de721b61d604614fdc516be
Earl Tilluck <earltilluck@hotmail.com>
2019-02-17 00:44:44 -0400

    finished

--- README.md
+++ README.md
@@ -2,15 +2,7 @@
 A web application that allows you to unscramble words from a random selection of letters.  
 Useful for cheating scrabble, bookworm adventures and other word games. 
 
-### If you have suggestions on how to improve this project, I am very interested to know.
-
-### todo:
-* add code for errors (/search/nothing or /search/1234)
-* display 20 items per page (js or bootstrap carousel)
-* Update web page (css)
-* improve algorithm?
-* read and check on how to avoid blocking.
-* how to stress test application?
+#### If you have suggestions on how to improve this project, I am very interested to know.
 
 ### Instructions
 This is a node server application that uses express
@@ -22,15 +14,21 @@ visit http://localhost:5000/ in dev environment.
 also available live @ [~~This website (not live yet~~)](https://www.google.com)
 
 ### Why?
-It is an interesting problem that may require a data structure and some clever thinking.  
-I also wanted to cheat my favourite word game.
+* It is an interesting problem that may require a data structure and some clever thinking.  
+* I also wanted to cheat my favourite word game.
 
 ### How?
-Instead of using a database, I keep a two dimensional array of [words][letters] in memory for searching.  
+I consider various data structures, but ultimatley decide to use a two dimensional array of [words][letters] in memory for searching. I try to keep the application light.  
 
 ### What I learned...
-* reading files with fs
-* promises
+* NodeJS, SASS
+
+### Future Improvements
+> I may get back to this in the future, but for now I am done.
+* A better algorithm or data structure to ease stress on server. (would a database help?)
+* Will this app block users on high traffic?
 
 ### More
-Original TWL06 text file downloaded from [Here](https://opusthepenguin.wordpress.com/2011/05/19/sowpods-vs-twl/)
+>TWL06 is a dictionary of approved scrabble words that can be used in USA scrabble tournaments.
+
+The TWL06 text file was retrieved from [Here](https://opusthepenguin.wordpress.com/2011/05/19/sowpods-vs-twl/)
--- public/script.js
+++ public/script.js
@@ -1,4 +1,96 @@
 
+// track current page (soft page)
+var page = 1;
+
+/**
+ * @desc showpage function paginates the
+ * results and views them 15 at a time.
+ * 
+ */
+function showpage() {
+    // set all results to hidden
+    var results = $('#results > div');
+    results.addClass("hidden");
+    // show results for current page range,
+    // e.g. pg1 = 0..14, pg2 = 15..29
+    var len = results.length;
+    var start = ((page-1)*15);
+    var end = start + 14; 
+    for (i=start; i<=end; i++) {
+        if(i>=0 && i<len) {
+            results.eq(i).removeClass("hidden");
+        }
+    }
+    // show current page
+    var totalpages = Math.ceil(((len-1)/15));
+    if(totalpages>0) {
+        $("#pg").html(
+            "page " + page + " of " + totalpages
+        );
+    }
+    console.log((start) + " ... " + (end));
+    // disable relevant button if on first or last page.
+    var prevbtn = $("#prev");
+    var nextbtn = $("#next");
+    prevbtn.addClass("invis");
+    nextbtn.addClass("invis");
+    if (start>0) {
+        prevbtn.removeClass("invis");
+    }
+    if (end<len-1) {
+        nextbtn.removeClass("invis");
+    }
+}
+
+/**
+ * @desc next and prev functions set the page
+ * and calls the showpage function
+ * 
+ */
+function next() {
+    page++;
+    showpage();
+}
+
+function prev() {
+    page--;
+    showpage();
+}
+
+/**
+ * @desc    shows an error on page,
+ * hides it after a while.
+ */
+var errTimer;
+function showError(error) {
+    clearTimeout(errTimer);
+    var err = $("#err");
+    err.html(error);
+    err.animate({ opacity: 1 })
+    errTimer = setTimeout(function() {
+        err.animate({ opacity: 0 });
+    }, 3000);
+}
+
+
+/**
+ * @desc appends a result to the result div (list)
+ * @param string res    a matched word result
+ * @param number len    length of the matched word 
+ */
+function appendResult(res, len) {
+    var elem = document.createElement("div");
+    var p = document.createElement("p");
+    if(len > 0) {
+        res = res + " (" + len +")";
+    }
+    p.innerHTML = res;
+    elem.appendChild(p);
+    $(elem).addClass("hidden");
+    $("#results").append(elem);
+}
+
+
 /**
  * display results on page, as well as time it took
  * 
@@ -11,19 +103,17 @@ function showResults(start, results) {
     // loop and append results as div, adding the length of each word.
     var len = results.length - 1;
     for(i=0; i<len; i++) {
-        var elem = document.createElement("div");
-        var p = document.createElement("p");
-        p.innerHTML = results[i] + " (" + results[i].length +")";
-        elem.appendChild(p);
-        $("#results").append(elem);
+        appendResult(results[i], results[i].length);
     }
     // show stats (time taken)    
     var end = new Date();
+    var total = results.length-1;
     $("#stats").html(
-        (results.length-1) +
-        " results in: " + results[results.length-1] + 
-        "ms on server, " + (end-start) + "ms total"
+        total + " results in: " + results[results.length-1] + "ms on server, " + (end-start) + "ms total."
     );
+    // display first page
+    page = 1;
+    showpage();
 }
 
 /**
@@ -32,23 +122,35 @@ function showResults(start, results) {
  * @param string inpt   search string
  */
 function search(inpt) {
-    // time tracking
-    var start = new Date();
-    
-    // request results from -domain-/search/inpt
-    var jqxhr = $.ajax({ 
-        url: "/search/" + inpt, 
-        dataType: 'json'
-    })
-    .done(function(data) {
-        showResults(start, JSON.parse(data));
-    })
-    .fail(function(jqXHR, textStatus, errorThrown) {
-        console.log("search error: " + errorThrown);
-    });
-
+    //clear error if still visible
+    showError("");
+    // don't search if letters are less than 3
+    if(inpt.length > 2) {
+        // time tracking
+        var start = new Date();
+        
+        // request results from -domain-/search/inpt
+        var jqxhr = $.ajax({ 
+            url: "/search/" + inpt, 
+            dataType: 'json'
+        })
+        .done(function(data) {
+            showResults(start, JSON.parse(data));
+        })
+        .fail(function(jqXHR, textStatus, errorThrown) {
+            console.log("search error: " + errorThrown);
+        });
+    } else {
+        showError("3 letters or more required.");
+    }
 }
-
+/**
+ * @desc    perform a search after 2 seconds.
+ * When the user is typing text, we want to 
+ * perform a search, only when the user is done.
+ * Prevents firing the event multiple times for
+ * each keyup trigger.
+ */
 var timer;
 function getResultsDelayed() {
     clearTimeout(timer);
@@ -69,5 +171,13 @@ $(document).ready(function() {
     $('#search-input').keyup(function() {
         getResultsDelayed();
     }); 
-    $('#fdate').html(new Date().getFullYear());   
+    $('#fdate').html(new Date().getFullYear());
+    
+    $('#next').click(function() {
+        next();
+    });
+    $('#prev').click(function() {
+        prev();
+    });
+
 });
